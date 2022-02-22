98be9f3818f5c3d1cc52b51039144a8017189796
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2022-02-22 15:23:25 -0800

    install ejs, create login ejs file and setup login get and post routes

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'));
+app.use(express.urlencoded({extended:true}));
+
 
 app.get('/',(req,res)=> {
    res.send('Home Page!!!');
 });
+
+app.get('/login', (req,res)=> {
+   res.render('login.ejs')
+})
+
+app.post('/login', (req,res)=>{
+   res.send(req.body);
+})
 
 app.listen(3000, ()=> console.log('Port 3000'));
