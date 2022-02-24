4a111c8da634f9f4f1944e12407ec821b9af7a47
Sunil Peddamalli <sunilpeddamalli@gmail.com>
2022-02-23 18:33:15 -0800

    create login get/post route and compare login password with hash password in DB by looking up the user in DB using login username

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const User = require('./modals/user');
 const bcrypt = require('bcrypt');
+const { is } = require('express/lib/request');
 
 mongoose.connect('mongodb://localhost/bcrypt')
     .then(()=>{
@@ -16,7 +17,6 @@ mongoose.connect('mongodb://localhost/bcrypt')
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'));
 app.use(express.urlencoded({extended:true}));
-
 
 app.get('/',(req,res)=> {
    res.send('Home Page!!!');
@@ -35,5 +35,16 @@ app.post('/register', async(req,res)=>{
     res.redirect('/')
 })
 
+app.get('/login', (req,res)=>{
+    res.render('login.ejs');
+});
+
+app.post('/login', async(req,res)=> {
+    let {username, password} = req.body;
+    const user = await User.findOne({username})
+    const isValidUser = await bcrypt.compare(password, user.password);
+    if(!isValidUser) return res.redirect('/login');
+    return res.redirect('/')
+});
 
 app.listen(3000, ()=> console.log('Port 3000'));
